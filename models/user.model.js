81cbf3bb5b0fa738a9f2e2598598b3cf9a36cdbd
Victor Erike <verike7@gmail.com>
2023-02-20 23:54:17 +0100

    Updated: Removed confirm password feat

--- models/user.model.js
+++ models/user.model.js
@@ -24,10 +24,7 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
-    confirmPassword: {
-        type: String,
-        // required: true,
-    },
+
 })
 
 // Define user validator function
@@ -37,39 +34,12 @@ const userValidator = async (user = {}) => {
         username: Joi.string().alphanum().min(5).max(30).required(),
         role: Joi.string().lowercase().valid('guest', 'admin'),
         password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).min(8).required(),
-        // confirmPassword: Joi.ref('password').required(),
     });
 
     const value = await schema.validateAsync(user);
 
     return value;
 }
-
-// Define static metod to be used on user object
-userSchema.methods.generateAuthToken = function t() {
-    const token = jwt.sign({
-        _id: this._id,
-        username: this.username,
-        role: this.role,
-    }, process.env.JWT_SECRET, {expriresIn: '7 days'});
-
-    return token;
-}
-
-userSchema.methods.toJSON = function toJSON (){
-    const userObject = this.toObject();
-    delete userObject.password;
-
-    return userObject;
-};
-
-userSchema.set('toJSON', {
-    versionKey: false,
-    transform(doc, ret) {
-        // 
-        delete ret.password;
-    }
-});
 
 userSchema.pre('save', async function preSave (next) {
     if (this.isModified('password') || this.isNew) {
