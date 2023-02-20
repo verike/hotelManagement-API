dd0d82c9a78fd80c467a7e82eebb375113268015
Victor Erike <verike7@gmail.com>
2023-02-20 18:39:22 +0100

    Updated: UserValidation function

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 
+// Define user schema
 const userSchema = new Schema({
     username: {
         type: String,
@@ -11,9 +14,10 @@ const userSchema = new Schema({
     },
     role: {
         type: String,
+        lowercase: true,
         required: true,
         trim: true,
-        // enum: ['guest', 'admin'],
+        enum: ['guest', 'admin'],
         default: 'guest',
     },
     password: {
@@ -22,17 +26,57 @@ const userSchema = new Schema({
     },
     confirmPassword: {
         type: String,
-        required: true,
+        // required: true,
     },
 })
 
+// Define user validator function
+const userValidator = async (user = {}) => {
 
-const userValidator = Joi.object({
-    username: Joi.string().alphanum().min(5).max(30).required().message('Username Required'),
-    role: Joi.string().valid('guest', 'admin'),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).min(8).required().message('Minimum of 8 characters'),
-    confirmPassword: Joi.ref('password').required().message('Confirm password'),
-})
+    const schema = Joi.object({
+        username: Joi.string().alphanum().min(5).max(30).required(),
+        role: Joi.string().lowercase().valid('guest', 'admin'),
+        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).min(8).required(),
+        // confirmPassword: Joi.ref('password').required(),
+    });
+
+    const value = await schema.validateAsync(user);
+
+    return value;
+}
+
+// Define static metod to be used on user object
+userSchema.methods.generateAuthToken = function t() {
+    const token = jwt.sign({
+        _id: this._id,
+        username: this.username,
+        role: this.role,
+    }, process.env.JWT_SECRET, {expriresIn: '7 days'});
+
+    return token;
+}
+
+userSchema.methods.toJSON = function toJSON (){
+    const userObject = this.toObject();
+    delete userObject.password;
+
+    return userObject;
+};
+
+userSchema.set('toJSON', {
+    versionKey: false,
+    transform(doc, ret) {
+        // 
+        delete ret.password;
+    }
+});
+
+userSchema.pre('save', async function preSave (next) {
+    if (this.isModified('password') || this.isNew) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
 
 const userModel = mongoose.model('user', userSchema);
