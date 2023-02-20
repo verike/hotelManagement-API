8f7af96f8a67ecd7cf44508b26b5500617c2a0cc
Victor Erike <verike7@gmail.com>
2023-02-20 18:40:15 +0100

    Updated: Added authentication

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Auth = require('../middlewares/authentication.middleware');
 
 const {
     createUser,
@@ -7,12 +8,24 @@ const {
     deleteUser,
     fetchOne,
     fetchAll,
+    login,
 } = require('../controllers/User.controller');
 
-router.post('/', createUser);
-router.get('/:id', fetchOne);
-router.patch('/:id', editUser);
-router.delete('/:id', deleteUser);
-router.get('/', fetchAll);
+router.route('/')
+    .get(fetchAll)
+    .post(createUser)
+
+router.route('/:id').all()
+    .get(fetchOne)
+    .patch([Auth.authenticate], editUser)
+    .delete([Auth.authenticate], deleteUser)
+
+router.post('/login', login)
+
+// router.post('/', createUser);
+// router.get('/:id', fetchOne);
+// router.patch([Auth.authenticate],'/:id', editUser);
+// router.delete([Auth.authenticate],'/:id', deleteUser);
+// router.get('/', fetchAll);
 
 module.exports = router;
